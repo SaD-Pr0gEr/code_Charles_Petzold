a5f63795dc274fe87843afef6f31179b75aab451
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-08-12 00:32:29 +0500

    chapter-1(morze-alphabite)

---
+++ ch1/notes.py
@@ -0,0 +1,23 @@
+"""
+In this book term "Code" means a system of transmission information
+between people and machine
+
+Morse alphabet(1791–1872)
+    Every letter signed as sequence of dots and dash(.- -> A)
+
+    LATIN
+    A: .-         M: --
+    B: -...       N: -.
+    C: -.-.       O: ---
+    D: -..        P: .--.
+    E: .          Q: --.-
+    F: ..-.       R: .-.
+    G: --.        S: ...
+    H: ....       T: -
+    I: ..         U: ..-
+    J: .---       V: ...-
+    K: -.-        W: .--
+    L: .-..       X: -..-
+                  Y: -.--
+                  Z: --..
+"""
